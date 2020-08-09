0d780fff166eb5e96d7620f75931d0f9c3b69279
James Pereira <james.pereira90@gmail.com>
2020-08-09 16:42:10 +1000

    Updates

---
+++ precache-manifest.0229302fd554a3d5a8fa962672ade3b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65f700b5ddeeeabbff8392dd7d571f41",
+    "url": "/jamespereira/jamespereira.github.io-todo-list/index.html"
+  },
+  {
+    "revision": "86765e59495a6f2d57c8",
+    "url": "/jamespereira/jamespereira.github.io-todo-list/static/css/main.06685e6e.chunk.css"
+  },
+  {
+    "revision": "aa6adb2f0885acf8f95d",
+    "url": "/jamespereira/jamespereira.github.io-todo-list/static/js/2.cf5db4cc.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/jamespereira/jamespereira.github.io-todo-list/static/js/2.cf5db4cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86765e59495a6f2d57c8",
+    "url": "/jamespereira/jamespereira.github.io-todo-list/static/js/main.d3fd39ef.chunk.js"
+  },
+  {
+    "revision": "a3d051e997936082b4e4",
+    "url": "/jamespereira/jamespereira.github.io-todo-list/static/js/runtime-main.add25604.js"
+  }
+]);
